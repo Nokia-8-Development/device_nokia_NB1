54771166db591ae89119b1a98e8b7e32e4c145eb
Edward Wu <bluehome.wu@gmail.com>
2021-09-20 02:44:15 +0800

    NB1: Track dotOS correct common stuff

--- dot_NB1.mk
+++ dot_NB1.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from NB1 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
